f28d2db0adad15fe646b90714af229aac998772e
luxuspur <luxuspur@gmail.com>
2024-10-22 14:13:58 +0000

    testing from idx

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "unstable";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.bun
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
